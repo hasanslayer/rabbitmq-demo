dbbd91bd444ffc6fed5437d1188b3e98389ec1dd
hasanslayer <hasan-dr2010@hotmail.com>
2023-08-24 17:26:11 +0300

    Add project files.

---
+++ RabbitSender/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitSender // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            factory.ClientProvidedName = "Rabbit Sender App";
+
+            IConnection cnn = factory.CreateConnection();
+
+            IModel channel = cnn.CreateModel();
+
+            string exchangeName = "DemoExchange";
+            string routingKey = "demo-routing-key";
+            string queueName = "DemoQueue";
+
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            channel.QueueDeclare(queueName, false, false, false, null);
+            channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+            byte[] messagebodyBytes = Encoding.UTF8.GetBytes("hello YouTube Again");
+            channel.BasicPublish(exchangeName, routingKey, null, messagebodyBytes);
+
+
+            channel.Close();
+            cnn.Close();
+        }
+    }
+}
